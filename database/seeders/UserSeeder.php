51a5034f95f1dc48a9525c17cae21341aadb4033
rohitgautamofficialmail@gmail.com <rohitgautamofficialmail@gmail.com>
2022-05-06 11:37:46 +0530

    home

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,51 +19,51 @@ class UserSeeder extends Seeder
             'username'  => 'Admin',
             'email' => 'admin@admin.com',
             'contact' => '1234567890',
-            'role_id' => '1',
+            'role_id' => '4',
             'password' => bcrypt('password')
         ]);
 
-        DB::table('users')->insert([
-            'username'  => 'Manager',
-            'email' => 'manager@gmail.com',
-            'contact' => '1234566541',
-            'role_id' => '2',
-            'password' => bcrypt('password')
-        ]);
+        // DB::table('users')->insert([
+        //     'username'  => 'Manager',
+        //     'email' => 'manager@gmail.com',
+        //     'contact' => '1234566541',
+        //     'role_id' => '2',
+        //     'password' => bcrypt('password')
+        // ]);
 
-        DB::table('users')->insert([
-            'username'  => 'Anshul Kumar',
-            'email' => 'anshul@gmail.com',
-            'contact' => '5486567890',
-            'role_id' => '3',
-            'password' => bcrypt('password')
-        ]);
-
-
-        DB::table('users')->insert([
-            'username'  => 'Mohit',
-            'email' => 'mohilt@gmail.com',
-            'contact' => '1234566541',
-            'role_id' => '3',
-            'password' => bcrypt('password')
-        ]);
-
-        DB::table('users')->insert([
-            'username'  => 'Dimple',
-            'email' => 'dimple@gmail.com',
-            'contact' => '1234566541',
-            'role_id' => '3',
-            'password' => bcrypt('password')
-        ]);
+        // DB::table('users')->insert([
+        //     'username'  => 'Anshul Kumar',
+        //     'email' => 'anshul@gmail.com',
+        //     'contact' => '5486567890',
+        //     'role_id' => '3',
+        //     'password' => bcrypt('password')
+        // ]);
 
 
-        DB::table('users')->insert([
-            'username'  => 'Raj',
-            'email' => 'raj@gmail.com',
-            'contact' => '1234566541',
-            'role_id' => '3',
-            'password' => bcrypt('password')
-        ]);
+        // DB::table('users')->insert([
+        //     'username'  => 'Mohit',
+        //     'email' => 'mohilt@gmail.com',
+        //     'contact' => '1234566541',
+        //     'role_id' => '3',
+        //     'password' => bcrypt('password')
+        // ]);
+
+        // DB::table('users')->insert([
+        //     'username'  => 'Dimple',
+        //     'email' => 'dimple@gmail.com',
+        //     'contact' => '1234566541',
+        //     'role_id' => '3',
+        //     'password' => bcrypt('password')
+        // ]);
+
+
+        // DB::table('users')->insert([
+        //     'username'  => 'Raj',
+        //     'email' => 'raj@gmail.com',
+        //     'contact' => '1234566541',
+        //     'role_id' => '3',
+        //     'password' => bcrypt('password')
+        // ]);
 
     }
 }
